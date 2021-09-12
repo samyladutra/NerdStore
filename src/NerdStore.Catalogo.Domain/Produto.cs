52c788c39a302d61159c184d284a6d41444de131
samyl <samyl@DESKTOP-U909D35>
2021-09-12 18:14:47 -0300

    Add project files.

---
+++ src/NerdStore.Catalogo.Domain/Produto.cs
@@ -0,0 +1,108 @@
+﻿using NerdStore.Core.DomainObject;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdStore.Catalogo.Domain
+{
+    //DDD:
+    //As entidades tem uma identidade definida (consegue comparar ela com base no Id)
+    //Todas as propriedades possuem o set privado
+    //Propriedades ADHOC
+    //Possui método de validação 
+
+    //IAggregateRoot é uma interface de marcação. Utilizada para saber se a classe que a implementa é uma raiz de agregação ou não
+    public class Produto : Entity, IAggregateRoot
+    {
+        public Guid CategoriaId { get; private set; }
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public bool Ativo { get; private set; }
+        public decimal Valor { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+        public string Imagem { get; private set; }
+        public int QuantidadeEstoque { get; private set; }
+        public Categoria Categoria { get; private set; }
+
+        public Produto(string nome, string descricao, bool ativo, decimal valor, Guid categoriaId, DateTime dataCadastro, string imagem, int quantidadeEstoque)
+        {
+            CategoriaId = categoriaId;
+            Nome = nome;
+            Descricao = descricao;
+            Ativo = ativo;
+            Valor = valor;
+            DataCadastro = dataCadastro;
+            Imagem = imagem;
+
+            //Não vai nem criar o objeto. (falhe primeiro)
+            Validar();
+        }
+
+        //AD HOC Setters seta de forma automática a propriedade. Evitando que altere o campo diretamente
+        public void Ativar() => Ativo = true;
+
+        public void Desativar() => Ativo = false;
+
+        public void AlterarCategoria(Categoria categoria)
+        {
+            Categoria = categoria;
+            CategoriaId = categoria.Id;
+        }
+
+        public void AlterarDescricao(string descricao)
+        {
+            Validacoes.ValidarSeVazio(descricao, "O campo Descricao do produto não pode estar vazio");
+            Descricao = descricao;
+        }
+
+        public void DebitarEstoque(int quantidade)
+        {
+            //transformando quantidade em positivo caso for negativo
+            if (quantidade < 0) quantidade *= -1;
+            if (QuantidadeEstoque < quantidade) throw new DomainException("Estoque insuficiente");
+            QuantidadeEstoque -= quantidade;
+        }
+
+        public void ReporEstoque(int quantidade)
+        {
+            QuantidadeEstoque += quantidade;
+        }
+
+        public bool PossuiEstoque(int quantidade)
+        {
+            return QuantidadeEstoque >= quantidade;
+        }
+
+        public void Validar()
+        {
+            Validacoes.ValidarSeVazio(Nome, "O campo Nome do produto não pode estar vazio");
+            Validacoes.ValidarSeVazio(Descricao, "O campo Descricao do produto não pode estar vazio");
+            Validacoes.ValidarSeDiferente(CategoriaId, Guid.Empty, "O campo CategoriaId do produto não pode ser vazio");
+            Validacoes.ValidarSeMenorIgualMinimo(Valor, 0, "O campo Valor do produto não pode ser menor igual ao minimo");
+            Validacoes.ValidarSeVazio(Imagem, "O campo Imagem do produto não pode estar vazio");
+        }
+    }
+
+    public class Categoria : Entity
+    {
+        public string Nome { get; private set; }
+        public int Codigo { get; private set; }
+
+        public Categoria(string nome, int codigo)
+        {
+            Nome = nome;
+            Codigo = codigo;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Codigo}";
+        }
+
+        public void Validar()
+        {
+            Validacoes.ValidarSeVazio(Nome, "O campo Nome da categoria não pode estar vazio");
+            Validacoes.ValidarSeIgual(Codigo, 0, "O campo Codigo da categoria não pode estar vazio");
+        }
+    }
+}
