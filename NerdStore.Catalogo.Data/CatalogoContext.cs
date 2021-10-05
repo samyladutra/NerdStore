efd724bd9f88e9190498b10c2c631bb9a0d65261
Samyla de Souza Dutra <samyladutra@gmail.com>
2021-10-05 20:52:18 -0300

    Implementação repositório de produto (camada acesso a dados)

--- NerdStore.Catalogo.Data/CatalogoContext.cs
+++ NerdStore.Catalogo.Data/CatalogoContext.cs
@@ -27,9 +27,21 @@ namespace NerdStore.Catalogo.Data
                 property.SetColumnType("varchar(100)");
             base.OnModelCreating(modelBuilder);
         }
-        public Task<bool> Commit()
+        public async Task<bool> Commit()
         {
-            throw new NotImplementedException();
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+            }
+            return await base.SaveChangesAsync() > 0;
         }
     }
 }
