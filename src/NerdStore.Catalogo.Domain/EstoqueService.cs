1f683e71139b515db3609cb6125595b0ab1f49e4
Samyla de Souza Dutra <samyladutra@gmail.com>
2021-10-05 21:33:30 -0300

    Add EstoqueService

---
+++ src/NerdStore.Catalogo.Domain/EstoqueService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class EstoqueService : IEstoqueService
+    {
+        private readonly IProdutoRepository _produtoRepository;
+        public EstoqueService(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+        public async Task<bool> DebitarEstoque(Guid produtoId, int quantidade)
+        {
+            var produto = await _produtoRepository.ObterPorId(produtoId);
+
+            //caso queira retornar exception pode ser feito tambem
+            if (produto == null) return false;
+
+            if (!produto.PossuiEstoque(quantidade)) return false;
+
+            produto.DebitarEstoque(quantidade);
+
+            _produtoRepository.Atualizar(produto);
+            return await _produtoRepository.UnitOfWork.Commit();
+        }
+        public async Task<bool> ReporEstoque(Guid produtoId, int quantidade)
+        {
+            var produto = await _produtoRepository.ObterPorId(produtoId);
+
+            if (produto == null) return false;
+            produto.ReporEstoque(quantidade);
+
+            _produtoRepository.Atualizar(produto);
+            return await _produtoRepository.UnitOfWork.Commit();
+        }
+
+        //TODO: caso tivesse mais ações como adquirir produto do fornecedor etc aqui seria um bom local para implementar
+        public void Dispose()
+        {
+            _produtoRepository.Dispose();
+        }
+    }
+}
