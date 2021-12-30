ab7d7f53ab6afe37b23850c4ce8f7e8f74a21eed
Samyla de Souza Dutra <samyladutra@gmail.com>
2021-12-30 20:55:20 -0300

    Primeiros passos Command e CommandHandler

--- src/NerdStore.Vendas.Domain/Voucher.cs
+++ src/NerdStore.Vendas.Domain/Voucher.cs
@@ -1,7 +1,8 @@
-﻿using NerdStore.Core.DomainObject;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+using FluentValidation;
+using FluentValidation.Results;
+using NerdStore.Core.DomainObject;
 
 namespace NerdStore.Vendas.Domain
 {
@@ -24,6 +25,34 @@ namespace NerdStore.Vendas.Domain
         internal ValidationResult ValidarSeAplicavel()
         {
             return new VoucherAplicavelValidation().Validate(this);
+        }
+    }
+
+    public class VoucherAplicavelValidation : AbstractValidator<Voucher>
+    {
+
+        public VoucherAplicavelValidation()
+        {
+            RuleFor(c => c.DataValidade)
+                .Must(DataVencimentoSuperiorAtual)
+                .WithMessage("Este voucher está expirado.");
+
+            RuleFor(c => c.Ativo)
+                .Equal(true)
+                .WithMessage("Este voucher não é mais válido.");
+
+            RuleFor(c => c.Utilizado)
+                .Equal(false)
+                .WithMessage("Este voucher já foi utilizado.");
+
+            RuleFor(c => c.Quantidade)
+                .GreaterThan(0)
+                .WithMessage("Este voucher não está mais disponível");
+        }
+
+        protected static bool DataVencimentoSuperiorAtual(DateTime dataValidade)
+        {
+            return dataValidade >= DateTime.Now;
         }
     }
 }
